f7d9453d49d69e5553ff6af7c2c20f349add33ee
Guilherme <guilherme_eco@hotmail.com>
2022-09-09 17:24:49 -0300

    fix to properly load css

--- calculoConsumoCombustivel/calculo.php
+++ calculoConsumoCombustivel/calculo.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_POST) {
+if ($_POST) {
 
 	$distancia = $_POST['distancia'];
 	$autonomia = $_POST['autonomia'];
@@ -11,51 +11,54 @@ if($_POST) {
 
 	$mensagem = "";
 
-	if (is_numeric($autonomia) && is_numeric($distancia)){
+	if (is_numeric($autonomia) && is_numeric($distancia)) {
 		if (($distancia > 0) && ($autonomia > 0)) {
 
-			$consumoGasolina = ($distancia / $autonomia ) * $valorGasolina;
-			$consumoGasolina = number_format($consumoGasolina,2,',','.');
+			$consumoGasolina = ($distancia / $autonomia) * $valorGasolina;
+			$consumoGasolina = number_format($consumoGasolina, 2, ',', '.');
 
-			$consumoAlcool = ($distancia / $autonomia ) * $valorAlcool;
-			$consumoAlcool = number_format($consumoAlcool,2,',','.');
+			$consumoAlcool = ($distancia / $autonomia) * $valorAlcool;
+			$consumoAlcool = number_format($consumoAlcool, 2, ',', '.');
 
-			$consumoDiesel = ($distancia / $autonomia ) * $valorDiesel;
-			$consumoDiesel = number_format($consumoDiesel,2,',','.');
+			$consumoDiesel = ($distancia / $autonomia) * $valorDiesel;
+			$consumoDiesel = number_format($consumoDiesel, 2, ',', '.');
 
-			$mensagem.= "<div class='sucesso'>";
-			$mensagem.= "O valor total gasto será de:";
-			$mensagem.= "<ul>";
-			$mensagem.= "<li><b>Gasolina:</b> R$ ".$consumoGasolina."</li>";
-			$mensagem.= "<li><b>Álcool:</b> R$ ".$consumoAlcool."</li>";
-			$mensagem.= "<li><b>Diesel</b>: R$ ".$consumoDiesel."</li>";
-			$mensagem.= "</ul>";
-			$mensagem.= "</div>";
+			$mensagem .= "<div class='sucesso'>";
+			$mensagem .= "O valor total gasto será de:";
+			$mensagem .= "<ul>";
+			$mensagem .= "<li><b>Gasolina:</b> R$ " . $consumoGasolina . "</li>";
+			$mensagem .= "<li><b>Álcool:</b> R$ " . $consumoAlcool . "</li>";
+			$mensagem .= "<li><b>Diesel</b>: R$ " . $consumoDiesel . "</li>";
+			$mensagem .= "</ul>";
+			$mensagem .= "</div>";
 		} else {
-			$mensagem.= "<div class='erro'>";
-			$mensagem.= "<b>O valor da distância e da autonomia deve ser maior que zero.</b>";
-			$mensagem.= "</div>";
+			$mensagem .= "<div class='erro'>";
+			$mensagem .= "<b>O valor da distância e da autonomia deve ser maior que zero.</b>";
+			$mensagem .= "</div>";
 		}
 	} else {
-		$mensagem.= "<div class='erro'>";
-		$mensagem.= "<b>O valor recebido não é numérico</b>";
-		$mensagem.= "</div>";
+		$mensagem .= "<div class='erro'>";
+		$mensagem .= "<b>O valor recebido não é numérico</b>";
+		$mensagem .= "</div>";
 	}
 } else {
 	$mensagem = "<div class='erro'>";
-	$mensagem.= "<b>Nenhum dado foi recebido pelo formulário</b>";
-	$mensagem.= "</div>";
+	$mensagem .= "<b>Nenhum dado foi recebido pelo formulário</b>";
+	$mensagem .= "</div>";
 }
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../css/styleComb.css">
 	<title>Calculo de Consumo de Combustível</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
 	<main>
 		<div class="painel">
@@ -69,4 +72,5 @@ if($_POST) {
 		</div>
 	</main>
 </body>
+
 </html>
